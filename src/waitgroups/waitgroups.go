5fc2d8f95a81399b281a3f74d3966291a04ceff9
hatellezp <hatellezp@gmail.com>
2024-11-04 11:30:37 +0100

    new example hopefully

--- src/waitgroups/waitgroups.go
+++ src/waitgroups/waitgroups.go
@@ -1,10 +1,30 @@
-package main 
+package main
 
- import (
+import (
 	"fmt"
+	"sync"
+	"time"
 )
 
-func main() {
-	fmt.Println("Hello World!");
+func worker(id int) {
+	fmt.Printf("worker %d starting\n", id)
+
+	time.Sleep(time.Second)
+	fmt.Printf("worker %d done\n", id)
 }
 
+func main() {
+	var wg sync.WaitGroup
+
+	for i := 1; i <= 5; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+			worker(i)
+		}()
+	}
+
+	wg.Wait()
+
+}
